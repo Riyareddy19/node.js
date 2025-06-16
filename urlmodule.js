83b8b6832655826d055ea3a6f8ac1134f82e6e1e
Riyareddy19 <ria.kasireddy@gmail.com>
2025-06-16 13:08:06 +0530

    file

---
+++ urlmodule.js
@@ -0,0 +1,10 @@
+import url from 'url';
+
+const myURL = new URL('https://example.org');
+myURL.pathname = '/a/b/c';
+myURL.search = '?d=e';
+myURL.hash = '#fgh';
+myURL.port = 8000;
+
+console.log(myURL);
+console.log(myURL.href);
